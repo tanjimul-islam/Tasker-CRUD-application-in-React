aef42a1046bb01e1e9ec11ee2d06cf1cac5da0eb
Tanjimul Islam <tanjimul.islam@hotmail.com>
2024-09-20 23:25:47 +0600

    splitted into components

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import TaskBoard from "./components/TaskBoard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col items-center">
+        <Hero />
+        <TaskBoard />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
